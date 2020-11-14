ebad1255b918244698381d62affaed326bb076df
wangyiman <2437990784@qq.com>
2020-11-14 09:36:32 +0800

    跳转（新）

--- starbucks/src/com/cugb/javaee/starbucks/action/LoginControl.java
+++ starbucks/src/com/cugb/javaee/starbucks/action/LoginControl.java
@@ -33,8 +33,8 @@ public class LoginControl extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
+		doPost( request,response);
+		}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
