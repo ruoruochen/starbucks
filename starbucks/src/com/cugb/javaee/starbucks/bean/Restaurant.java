715f99817e44dbdaa57cfa2fa59bcfaf6801a2f1
Chen Huan <2679330388@qq.com>
2020-11-14 11:31:35 +0800

    Restaurant.java

---
+++ starbucks/src/com/cugb/javaee/starbucks/bean/Restaurant.java
@@ -0,0 +1,20 @@
+package com.cugb.javaee.starbucks.bean;
+
+public class Restaurant {
+	public String restaurantid;
+	public String restaurantname;
+	
+	public String getRestaurantname() {
+		return restaurantname;
+	}
+	public void setRestaurantname(String res) {
+		this.restaurantname = res;
+	}
+	
+	public String getRestaurantid() {
+		return restaurantid;
+	}
+	public void setRestaurantid(String id) {
+		this.restaurantid = id;
+	}
+}
